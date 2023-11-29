7c0834b6eb5021afef75f4b069062553e68004c2
Ipikk <irvityara456@gmail.com>
2023-11-29 15:47:32 +0700

    user controller, route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -50,4 +50,51 @@ module.exports = {
       });
     }
   },
+  editUserById: async (req, res) => {
+    const { id } = req.params;
+    const { username, email, password, role } = req.body;
+
+    const index = await db.Todo.findByPk(id);
+    db.Todo[index] = { id, username, email, password, role };
+
+    index.id = id || index.id;
+    index.username = username || index.username;
+    index.email = email || index.email;
+    index.password = password || index.password;
+    index.role = role || index.role;
+
+    await index.save();
+
+    res.json({
+      message: "berhasil mengubah data user",
+      data: User,
+    });
+  },
+  deleteUserById: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const user = await db.User.findByPk(id);
+
+      if (!user) {
+        res.json({
+          message: "User not found.",
+        });
+      }
+
+      await User.destroy();
+
+      const users = await db.User.findAll();
+
+      res.json({
+        message: "Berhasil menghapus user by id",
+        data: users,
+      });
+    } catch {
+      res.json({
+        message: "Cannot delete user",
+        error: error.message,
+      });
+    }
+  },
 };
--- routes/user-route.js
+++ routes/user-route.js
@@ -4,9 +4,13 @@ const route = express.Router();
 const {
   getAllUser,
   getUserById,
+  editUserById,
+  deleteUserById,
 } = require("../controllers/user-controller");
 
 route.get("/", getAllUser);
 route.get("/:id", getUserById);
+route.put("/:id", editUserById);
+route.delete("/:id", deleteUserById);
 
 module.exports = route;

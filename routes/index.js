97f34bd3153c04f9f1ba01ba7e27353946108e0b
Ipikk <irvityara456@gmail.com>
2023-11-29 17:26:33 +0700

    route

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ route.get("/", (req, res) => {
     res.json("Selamat datang di aplikasi Konsultasi Dokter dan Informasi Kesehatan Bersama SEHATIN");
 });
   
-route.use("/users", userRoutes);
+route.use("/users", verifyToken, userRoutes);
 route.use("/auth", authRoutes);
-route.use("/dokter", verifyToken, dokterRoutes);
-route.use("/food", verifyToken, foodRoutes);
+route.use("/dokter", dokterRoutes);
+route.use("/food", foodRoutes);
 
 module.exports = route;
